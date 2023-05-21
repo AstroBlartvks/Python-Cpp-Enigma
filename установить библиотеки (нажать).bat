494afa28a7993cea078deec40b6a36d13b10eab4
Astro <98228094+AstroBlartvks@users.noreply.github.com>
2023-05-21 22:41:31 +0400

    Add files via upload

--- установить библиотеки (нажать).bat
+++ установить библиотеки (нажать).bat
@@ -6,5 +6,5 @@ pip install PyQt5
 echo Installing PyTorch
 pip install torch
 echo If you want to use a neural network, then you need to download a folder from Google disk, link:
-echo https://drive.google.com/drive/folders/14xHAm9PzU3Pc8C5d8SCKmeICrzCzrTc_?usp=sharing
+echo https://drive.google.com/drive/folders/14xHAm9PzU3Pc8C5d8SCKmeICrzCzrTc_
 pause
