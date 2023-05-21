253779c0b7a6dea8bb4350908b961132f80561fe
Astro <98228094+AstroBlartvks@users.noreply.github.com>
2023-05-21 21:22:59 +0400

    Add files via upload

--- установить библиотеки (нажать).bat
+++ установить библиотеки (нажать).bat
@@ -1,1 +1,10 @@
+@chcp 1251
+@echo off
+echo Library installer AstroEnigma
+echo Installing PyQt5
 pip install PyQt5
+echo Installing PyTorch
+pip install torch
+echo If you want to use a neural network, then you need to download a folder from Google disk, link:
+echo https://drive.google.com/drive/folders/14xHAm9PzU3Pc8C5d8SCKmeICrzCzrTc_?usp=sharing
+pause
